52cc82f70c5202842245eef9788860c885c73657
Diego Luna <Diego.LunaMendoza@alu.frt.utn.edu.ar>
2026-05-21 10:22:07 -0300

    Realizar CasoDictionary

--- Dsw2026Ej11/Collections/CasoDictionary.cs
+++ Dsw2026Ej11/Collections/CasoDictionary.cs
@@ -9,4 +9,21 @@ namespace Dsw2026Ej11.Collections;
 //Incluir un método para eliminar un alumno utilizando la clave
 public class CasoDictionary
 {
+    private readonly Dictionary<int, Alumno> dic = [];
+
+    //Metodo para agregar alumno 
+    public void AgregarAlumno(int legajo, Alumno alumno) => dic.Add(legajo, alumno);
+
+    //Metodo para buscar alumno por clave
+    public Alumno? BuscarPorClave(int legajo)
+    {
+        dic.TryGetValue(legajo, out Alumno? alumno);
+        return alumno;
+    }
+
+    //Metodo para retornar el diccionario
+    public Dictionary<int, Alumno> ObtenerDiccionario() => dic;
+
+    //Metodo para eliminar utilizando una clave 
+    public void EliminarAlumnoClave(int legajo) => dic.Remove(legajo);
 }
